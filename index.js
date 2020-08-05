80f8ff7ec6226600a30427b32e6e1a3b82d675b6
Abdellah Sabiri <abdellah.sabiri.1995@gmail.com>
2020-08-05 23:39:26 +0100

    Finally

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-const readme = fs.readFileSync('./README.md', 'utf-8');
-readme.replace("{{time}}", new Date().toString());
-fs.writeSync("./README.md", readme);
+let readme = fs.readFileSync('./README.md', 'utf-8');
+readme = readme.replace("{{time}}", new Date().toString());
+fs.writeFileSync("./README.md", readme);
