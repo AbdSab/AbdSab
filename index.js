c17b9ac23c754db622e12b55887e05c2c8ac1ab1
Abdellah Sabiri <abdellah.sabiri.1995@gmail.com>
2020-08-06 00:08:30 +0100

    Finally !

--- index.js
+++ index.js
@@ -26,6 +26,5 @@ const commitReadme = async () => {
   
 
 let readme = fs.readFileSync('./README.md', 'utf-8');
-readme = readme.replace("{{time}}", new Date().toString());
-fs.writeFileSync("./README.md", readme);
+fs.writeFileSync("./README.md", parseInt(readme)+1);
 commitReadme();
